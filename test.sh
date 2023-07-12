5a3db199e7b455cc9c57f853a8c117fde52a40ee
kimjioo <82633446+kimjioo@users.noreply.github.com>
2023-07-12 15:03:47 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,24 +7,24 @@ echo "----------------v1.1 By @信仰------------"
 echo "----------------v1.2 1.3 1.4 By @南梦宫------------"
 echo "----------------https://crackemby.mb6.top------------"
 echo "----------------web播放器开始------------"
-
+mv ~/Desktop/test/Emby.Web.dll ~/Desktop/test/Emby.Web.dll.bak
 wget -P ~/Desktop/test --no-check-certificate https://raw.githubusercontent.com/kimjioo/eeemmbby/main/Emby.Web.dll
-
+mv ~/Desktop/test/MediaBrowser.Model.dll ~/Desktop/test/MediaBrowser.Model.dll.bak
 wget -P ~/Desktop/test --no-check-certificate https://raw.githubusercontent.com/kimjioo/eeemmbby/main/MediaBrowser.Model.dll
 echo "----------------web核心完成------------"
-
+mv ~/Desktop/test/connectionmanager.js ~/Desktop/test/connectionmanager.js.bak
 wget -P ~/Desktop/test --no-check-certificate https://raw.githubusercontent.com/kimjioo/eeemmbby/main/connectionmanager.js
-
+mv ~/Desktop/test/embypremiere.js ~/Desktop/test/embypremiere.js.bak
 wget -P ~/Desktop/test --no-check-certificate https://raw.githubusercontent.com/kimjioo/eeemmbby/main/embypremiere.js
 echo "----------------web播放器完成------------"
 echo "----------------硬解部分开始----------------"
-
+mv ~/Desktop/test/Emby.Server.Implementations.dll ~/Desktop/test/Emby.Server.Implementations.dll.bak
 wget -P ~/Desktop/test --no-check-certificate https://raw.githubusercontent.com/kimjioo/eeemmbby/main/Emby.Server.Implementations.dll
 echo "----------------硬解部分完成-----------"
 echo "----------------设置相关权限（DSM6版本会报错，无影响）----------------"
-chown -R emby:emby /var/packages/EmbyServer/target/system/
+
 echo "-------------------删除脚本，预防二次运行出错-------------------"
-#rm -rf synology6-7crack_4.7.13.sh
+rm -rf test.sh
 echo "-------------------完成-------------------"
 echo "运行后重启套件,清除浏览器缓存,填写任意激活码."
 echo "运行后重启套件,清除浏览器缓存,填写任意激活码."
